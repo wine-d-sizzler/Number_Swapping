b76c3e8f8e6668ab9d9deb260659bb585db9e5f9
wine-d-sizzler <74056361+wine-d-sizzler@users.noreply.github.com>
2020-11-24 12:53:54 +0530

    Add files via upload

---
+++ Number Swapping Project-1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main() {
+      int first, second, temp;
+      printf("Enter first number: ");
+      scanf("%d", &first);
+      printf("Enter second number: ");
+      scanf("%d", &second);
+      // Value of first is assigned to temp
+      temp = first;
+      // Value of second is assigned to first
+      first = second;
+      // Value of temp (initial value of first) is assigned to second
+      second = temp;
+      printf("After swapping, the first number = %d\n", first);
+      printf("After swapping, secondNumber = %d", second);
+      return 0;
+}
